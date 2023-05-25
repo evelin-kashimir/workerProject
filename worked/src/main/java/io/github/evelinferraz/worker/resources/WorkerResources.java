40194bd55df853757c61e029fe6d462ee6082d9a
Evelin Ferraz <evelin.moitinho@capgemini.com>
2023-05-25 20:48:05 -0300

    [WORKER] - Configurando projeto worked para acesso por porta aleatória

---
+++ worked/src/main/java/io/github/evelinferraz/worker/resources/WorkerResources.java
@@ -0,0 +1,45 @@
+package io.github.evelinferraz.worker.resources;
+
+import java.util.List;
+
+
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.core.env.Environment;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+import io.github.evelinferraz.worker.entity.Worker;
+import io.github.evelinferraz.worker.repositories.WorkerRepository;
+
+@RestController
+@RequestMapping("/workers")
+public class WorkerResources {
+
+	private static Logger logger = LoggerFactory.getLogger(WorkerResources.class);
+	@Autowired
+	private Environment env;
+	
+	@Autowired
+	private WorkerRepository repository;
+	
+	@GetMapping
+	public ResponseEntity<List<Worker>> findAll(){
+		List<Worker> workers = repository.findAll();
+		return ResponseEntity.ok(workers);
+	}
+	
+	@GetMapping(value="/{id}")
+	public ResponseEntity<Worker> findById(@PathVariable Long id){
+		
+		//Imprime no log o número da porta que está rodando o projeto
+		logger.info("PORT ====> "+ env.getProperty("local.server.port"));
+		
+		Worker worker = repository.findById(id).get();
+		return ResponseEntity.ok(worker);
+	}	
+}
