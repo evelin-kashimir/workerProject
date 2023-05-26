b964663ce8fa40463a090c31221560bac35de9a8
Evelin Ferraz <evelin.moitinho@capgemini.com>
2023-05-25 21:54:21 -0300

    [WORKER] - Configurando api-gateway para roteamento de urls para os microserviços

--- hr-api-gateway/src/main/java/io/github/evelinferraz/hrapigateway/HrApiGatewayApplication.java
+++ hr-api-gateway/src/main/java/io/github/evelinferraz/hrapigateway/HrApiGatewayApplication.java
@@ -2,7 +2,11 @@ package io.github.evelinferraz.hrapigateway;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.cloud.netflix.eureka.EnableEurekaClient;
+import org.springframework.cloud.netflix.zuul.EnableZuulProxy;
 
+@EnableEurekaClient
+@EnableZuulProxy
 @SpringBootApplication
 public class HrApiGatewayApplication {
 
